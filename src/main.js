668602b65c4f0d7369066a83a89dd19c2de6d8f0
CraiG <zcg.1314@qq.com>
2019-07-24 21:33:25 +0800

    feat:添加Vuescroll

--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'element-ui/lib/theme-chalk/index.css';
-
+import vuescroll from 'vuescroll';
 Vue.use(ElementUI);
 Vue.config.productionTip = false
+
+Vue.use(vuescroll, {
+  ops: {
+    bar: {
+      onlyShowBarOnScroll: false,
+      background: '#c1c1c1',
+    }
+  },
+  name: 'Scroll'
+});
 
 new Vue({
   router,
