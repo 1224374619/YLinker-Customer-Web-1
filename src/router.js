8137e34e5951419ca41bf65ed7ca74953f9b595c
CraiG <zcg.1314@qq.com>
2019-07-24 21:33:51 +0800

    feat:配置子路由

--- src/router.js
+++ src/router.js
@@ -15,57 +15,66 @@ import www from './views/www.vue'
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: Register
-    },
-    {
-      path: '/reset-password',
-      name: 'reset-password',
-      component: ResetPassword
-    },
-    {
-      path: '/reset-result',
-      name: 'reset-result',
-      component: ResetResult
-    },
-    {
-      path: '/register-result',
-      name: 'register-result',
-      component: RegisterResult
-    },
-    {
-      path: '/user-license',
-      name: 'user-license',
-      component: UserLicense
-    },
-    {
-      path: '/search',
-      name: 'search',
-      component: Search
-    },
-    {
-      path: '/detail/:id',
-      name: 'detail',
-      component: JobDetail
-    },
-    {
-      path: '/www',
-      name: 'www',
-      component: www
-    }
-  ]
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'front',
+            component: resolve => require(['./layout/front'], resolve),
+            redirect: '/home',
+            children: [
+                {
+                    path: '/home',
+                    name: 'home',
+                    component: Home
+                },
+
+                {
+                    path: '/www',
+                    name: 'www',
+                    component: www
+                }
+            ]
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register
+        },
+        {
+            path: '/reset-password',
+            name: 'reset-password',
+            component: ResetPassword
+        },
+        {
+            path: '/reset-result',
+            name: 'reset-result',
+            component: ResetResult
+        },
+        {
+            path: '/register-result',
+            name: 'register-result',
+            component: RegisterResult
+        },
+        {
+            path: '/user-license',
+            name: 'user-license',
+            component: UserLicense
+        },
+        {
+            path: '/search',
+            name: 'search',
+            component: Search
+        },
+        {
+            path: '/detail/:id',
+            name: 'detail',
+            component: JobDetail
+        },
+    ]
 });
