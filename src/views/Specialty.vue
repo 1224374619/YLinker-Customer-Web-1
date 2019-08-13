959ac3da20727d4d7749dd46b23910a170ba2eb2
1224374619 <1224374619@qq.com>
2019-08-13 19:33:46 +0800

    8.13

---
+++ src/views/Specialty.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="specialty">
+    <div class="specialty-title">请填写你的基本信息:</div>
+    <div class="specialty-form">
+      <el-form
+        :model="formInline"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-formInline"
+        :inline="true"
+      >
+        <el-row :gutter="20">
+          <el-col :span="13">
+            <el-form-item label="姓名" prop="name" style="margin-left:10px">
+              <el-input style="width:168px;height:36px" v-model="formInline.name" placeholder="请输入姓名"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="11">
+            <img style="margin:-45px 0 0 0" :src="require('../assets/images/11.png')" />
+          </el-col>
+        </el-row>
+
+        <el-form-item label="性别">
+          <el-radio-group style="width:168px;height:36px" v-model="formInline.gender">
+            <el-row :gutter="20">
+              <el-col :span="12">
+                <el-radio-button label="男性"></el-radio-button>
+              </el-col>
+              <el-col :span="12">
+                <el-radio-button label="女性"></el-radio-button>
+              </el-col>
+            </el-row>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="最高学历">
+          <el-select style="width:168px;height:36px" v-model="formInline.educationLevel" placeholder>
+            <el-option label value="初中及以下"></el-option>
+            <el-option label value="中专/职中"></el-option>
+            <el-option label value="高中"></el-option>
+            <el-option label value="大专"></el-option>
+            <el-option label value="本科"></el-option>
+            <el-option label value="硕士"></el-option>
+            <el-option label value="博士"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="生日">
+          <el-date-picker
+            style="width:168px;height:36px"
+            type="date"
+            placeholder="选择日期"
+            v-model="formInline.birthday"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item label="海外工作年限">
+          <el-select style="width:168px;height:36px" v-model="formInline.workingSeniority" placeholder>
+            <el-option label value="无工作年限"></el-option>
+            <el-option label value="1-3年"></el-option>
+            <el-option label value="3-5年"></el-option>
+            <el-option label value="5-10年"></el-option>
+            <el-option label value="手动输入"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="工作年限">
+          <el-select style="width:168px;height:36px" v-model="formInline.workingSeniority" placeholder>
+            <el-option label value="无工作年限"></el-option>
+            <el-option label value="1-3年"></el-option>
+            <el-option label value="3-5年"></el-option>
+            <el-option label value="5-10年"></el-option>
+            <el-option label value="手动输入"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="政治面貌">
+          <el-select style="width:168px;height:36px" v-model="formInline.politicCountenance" placeholder>
+            <el-option label value="群众"></el-option>
+            <el-option label value="党员"></el-option>
+            <el-option label value="中共团员"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="手机">
+          <el-input style="width:168px;height:36px" v-model="formInline.phone" placeholder></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input style="width:168px;height:36px" v-model="formInline.email" placeholder></el-input>
+        </el-form-item>
+        <el-form-item style="margin:50px 0 40px 280px">
+          <el-button class="keep" @click="next" style="width:270px;height:43px" type="primary">下一步</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "specialty",
+  data() {
+    return {
+      formInline: {
+          name: "",
+          gender: "",
+          workingSeniority: "",
+          educationLevel: "",
+          birthday: "",
+          email: "",
+          phone: "",
+          politicCountenance: ""
+        },
+      
+    };
+  },
+  methods: {
+    cancel() {
+      const h = this.$createElement;
+      this.$msgbox({
+        message: h(
+          "div",
+          {
+            style: "height:200px"
+          },
+          [
+            h(
+              "p",
+              {
+                style: "font-size:20px;line-height:200px"
+              },
+              "当前编辑内容未完成，是否继续操作？ "
+            ),
+            h(
+              "p",
+              {
+                style: "font-size:14px;margin-top:-80px"
+              },
+              "退出编辑后，更新的内容不会自动保存 "
+            )
+          ]
+        ),
+        showCancelButton: true,
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    next() {
+        this.$router.push({path:'/resume'})
+    },
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.specialty
+  width 770px
+  background white
+  margin 90px 0 0 0
+  border 1px solid white 
+  .specialty-title
+    font-size 16px
+    color #61687c
+    text-align left  
+    margin 56px 0 0 50px
+  .specialty-form
+    width 600px
+    margin 50px 0 0 10px 
+</style>
