797893cea52f60025f96677b685abaed8174b353
1224374619 <1224374619@qq.com>
2019-09-24 20:03:22 +0800

    9.24

--- src/apis/account.js
+++ src/apis/account.js
@@ -10,8 +10,8 @@ export const brief = () => axios.get('api/resume/brief');
 //获取收藏的岗位
 export const favorite = () => axios.get('api/favorite/position');
 //取消对岗位的收藏
-export const isfavorite = (id) => axios.detele(`/favorite/position/${id}`);
+export const isfavorite = (id) => axios.delete(`api/favorite/position/${id}`);
 //城市
-export const city = () => axios.get('/constant/district');
+export const city = () => axios.get('api/constant/district');
 //获取推荐岗位
-export const searched = () => axios.post('/searched/position');
+export const searched = (params) => axios.post('api/searched/position',params);
